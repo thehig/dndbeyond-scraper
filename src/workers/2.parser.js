f7688f969cdb688f24c3cd503cb45f674a001805
David Higgins <thehig@gmail.com>
2020-01-01 18:34:51 +0000

    Start by parsing out some spells, counting prepared

--- src/workers/2.parser.js
+++ src/workers/2.parser.js
@@ -1,28 +1,30 @@
 const fs = require("fs-extra");
 const path = require("path");
 
-const objectStructure = ({ name, url, json }, options) => {
-  console.log(`${name} -- ${url}`);
-  const jsonData = JSON.parse(json);
+// SCOTTS GIST:       https://gist.github.com/TheHandsomeCoder/9ce9facf5ad4be6e3589755e08311025
+//
+// //Create set of spells by ID for quick compare
+// const mySpellSet = new Set(octaviansSpells.map(s => s.id));
 
-  const replacer = (key, value) => {
-    // Remove nulls
-    if (value === null) return undefined;
-    // Remove non-objects but leave their keys
-    if (typeof value !== "object") return "";
-    // Remove empty arrays
-    if (Array.isArray(value) && value.length === 0) return undefined;
-    // Remove empty objects
-    if (typeof value === "object" && Object.keys(value).length == 0)
-      return undefined;
+// //Helper methods, could just include Ramda
+// const sum = (a, c) => a + c;
+// const sumPagesUsed = (a, c) => a + c.definition.level;
+// const spellCost = (isNecromancy, spellLevel) => isNecromancy ? (spellLevel * 50) / 2 : spellLevel * 50
+// const spellTime = (isNecromancy, spellLevel) => isNecromancy ? (spellLevel * 2) / 2 : spellLevel * 2
 
-    return value;
-  };
+// const newSpells = zotrisSpells
+//     .filter(s => !mySpellSet.has(s.id)) //Find spells octavian doesn't have
+//     .filter(s => s.definition.level !== 0) // Can't learn cantrips so filter them
+//     .map(s => ({
+//         name: s.definition.name,
+//         school: s.definition.school,
+//         level: s.definition.level,
+//         cost: spellCost(s.definition.school === 'Necromancy', s.definition.level),
+//         time: spellTime(s.definition.school === 'Necromancy', s.definition.level)
+//     })); //Map only useful info for now
 
-  const structure = JSON.stringify(jsonData, replacer, 4);
-
-  console.log(structure);
-};
+// const totalCost = newSpells.map(s => s.cost).reduce(sum);
+// const totalTime = newSpells.map(s => s.time).reduce(sum);
 
 // Turn the scraped data into parsed, usable information
 async function main(paramData, options) {
@@ -37,12 +39,14 @@ async function main(paramData, options) {
 
   let data = paramData;
   if (READ_FROM_CACHE) {
-    SHOW_PROGRESS && console.log(`[ ]      READ_FROM_CACHE Ignoring paramData`);
+    SHOW_PROGRESS &&
+      console.log(`[ ]      READ_FROM_CACHE Ignoring passed parameter data`);
     const filesInCacheDir = await fs.readdir(CACHE_DATA_DIR);
 
     data = await Promise.all(
       filesInCacheDir
-        .filter(f => f.toLocaleLowerCase().endsWith("json"))
+        .filter(f => f.toLocaleLowerCase().endsWith("json")) // That ends with JSON
+        .filter(f => !f.toLocaleLowerCase().startsWith("_")) // Not underscore files
         .map(
           async filename =>
             await fs.readJSON(path.join(CACHE_DATA_DIR, filename))
@@ -56,33 +60,38 @@ async function main(paramData, options) {
     if (MAX_DATA_ROWS && maxCounter++ >= MAX_DATA_ROWS) {
       return;
     }
-    return objectStructure(character, options);
+    return parseCharacter(character, options);
   });
+}
 
-  // SCOTTS GIST:       https://gist.github.com/TheHandsomeCoder/9ce9facf5ad4be6e3589755e08311025
-  //
-  // //Create set of spells by ID for quick compare
-  // const mySpellSet = new Set(octaviansSpells.map(s => s.id));
+function parseCharacter(input, options) {
+  const { name, url, json } = input;
+  // console.log("Parsing", name);
+  if (name === undefined) {
+    debugger;
+  }
+  const {
+    character /* , characterConfiguration, characterData */
+  } = JSON.parse(json);
 
-  // //Helper methods, could just include Ramda
-  // const sum = (a, c) => a + c;
-  // const sumPagesUsed = (a, c) => a + c.definition.level;
-  // const spellCost = (isNecromancy, spellLevel) => isNecromancy ? (spellLevel * 50) / 2 : spellLevel * 50
-  // const spellTime = (isNecromancy, spellLevel) => isNecromancy ? (spellLevel * 2) / 2 : spellLevel * 2
+  if (character && character.spells) parseSpells(character);
+  // character.classSpells[0].spells
+}
 
-  // const newSpells = zotrisSpells
-  //     .filter(s => !mySpellSet.has(s.id)) //Find spells octavian doesn't have
-  //     .filter(s => s.definition.level !== 0) // Can't learn cantrips so filter them
-  //     .map(s => ({
-  //         name: s.definition.name,
-  //         school: s.definition.school,
-  //         level: s.definition.level,
-  //         cost: spellCost(s.definition.school === 'Necromancy', s.definition.level),
-  //         time: spellTime(s.definition.school === 'Necromancy', s.definition.level)
-  //     })); //Map only useful info for now
+const isPrepared = spell => spell.prepared === true;
 
-  // const totalCost = newSpells.map(s => s.cost).reduce(sum);
-  // const totalTime = newSpells.map(s => s.time).reduce(sum);
+function parseSpells(character, options) {
+  if (character.classSpells && character.classSpells.length > 0) {
+    // TODO: Support multiclassing spells
+    const spells = character.classSpells[0].spells;
+    const preparedSpells = spells.filter(isPrepared);
+
+    const { name: characterName, currentXp } = character;
+    if (preparedSpells.length)
+      console.log(
+        `   ${characterName} has ${preparedSpells.length} prepared spell(s)`
+      );
+  }
 }
 
 module.exports = main;
